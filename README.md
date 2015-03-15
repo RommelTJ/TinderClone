62f431ea951d2c510bfcc15f61435027a028843f
Rommel Rico <rommeltj@gmail.com>
2015-03-14 22:30:04 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# A Tinder Clone in Swift for iOS
+
+Version: 0.0.1 - 14 Mar 2015
+
+## Description
+
+Udemy iOS/Swift tutorials.
+
+### Documentation
+Branches:  
+master = Production branch  
+dev = Development branch  
+
+## Contact
+
+<me@rommelrico.com>
+
+## Notes
+
+If you want to contribute, email me at <me@rommelrico.com>.
---
+++ Tinder/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Rob Percival on 02/09/2014.
+//  Copyright (c) 2014 Appfish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var permissions = ["public_profile"]
+        
+        // Update - added , block:
+        
+        PFFacebookUtils.logInWithPermissions(permissions, block: {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user == nil {
+                NSLog("Uh oh. The user cancelled the Facebook login.")
+            } else if user.isNew {
+                NSLog("User signed up and logged in through Facebook!")
+            } else {
+                NSLog("User logged in through Facebook!")
+            }
+        })
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
